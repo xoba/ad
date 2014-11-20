b09429a1e91284a395abb43c528e3992a528d150
mike andrews <mra@xoba.com>
2014-11-20 18:21:24 -0500

    specifying neural network

--- src/xoba/nn/nn.go
+++ src/xoba/nn/nn.go
@@ -11,14 +11,21 @@ import (
 //go:generate run compile -formula=nn.txt -output nn_ad.go -templates "../ad/parser/templates" -package nn -main=false -time=false
 func Run(args []string) {
 	var gen bool
+	var nclass, hidden, inputs int
 	flags := flag.NewFlagSet("parse", flag.ExitOnError)
 	flags.BoolVar(&gen, "gen", false, "whether to generate formula")
+	flags.IntVar(&inputs, "inputs", 2, "number of inputs")
+	flags.IntVar(&nclass, "classes", 3, "number of classes")
+	flags.IntVar(&hidden, "hidden", 3, "number of hidden units")
 	flags.Parse(args)
-	fmt.Println("generating formula")
-	f, err := os.Create("nn.txt")
-	check(err)
-	defer f.Close()
-	fmt.Fprintf(f, "f := a+b\n")
+	if gen {
+		fmt.Println("generating formula")
+		f, err := os.Create("nn.txt")
+		check(err)
+		defer f.Close()
+		fmt.Fprintf(f, "f := a+b\n")
+		return
+	}
 }
 
 func check(e error) {
