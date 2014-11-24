b6e1b0be51d9eb2b50c74fd74241d154956ba82d
mike andrews <mra@xoba.com>
2014-11-24 12:11:26 -0500

    bigger IDE window

--- lib/x-ide.sh
+++ lib/x-ide.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PWD=`pwd`
-emacs `./lib/sourcefiles.sh` --geometry 120x70 --eval "(add-hook 'emacs-startup-hook 'delete-other-windows)" --title "`basename $PWD` ide" &
+emacs `./lib/sourcefiles.sh` --geometry 125x75 --eval "(add-hook 'emacs-startup-hook 'delete-other-windows)" --title "`basename $PWD` ide" &
