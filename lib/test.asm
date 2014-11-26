cd0e23ffc051d2d96b42f8676b234cad368eb974
mike andrews <mra@xoba.com>
2014-11-26 11:11:56 -0500

    filled in alot of the vmfuncs

--- lib/test.asm
+++ lib/test.asm
@@ -1,6 +1,6 @@
 # a test program
 registers 10
-outputs 2
+outputs 3
 inputs 1
 models 0
 literal 0 3.14
@@ -8,6 +8,8 @@ literal 1 2
 multiply 0 1 2
 exp 2 2
 pow 0 1 3
+d_multiply_d1 0 1 4
 setoutput 2 0
 setoutput 3 1
+setoutput 4 2
 halt
--- src/xoba/ad/vm/vmfuncs.go
+++ src/xoba/ad/vm/vmfuncs.go
@@ -7,84 +7,98 @@ func exp10(f float64) float64 {
 }
 
 func d_add_d0(a, b float64) float64 {
-	panic("unimplemented")
-}
-func d_divide_d0(a, b float64) float64 {
-	panic("unimplemented")
-}
-func d_multiply_d0(a, b float64) float64 {
-	panic("unimplemented")
-}
-func d_subtract_d0(a, b float64) float64 {
-	panic("unimplemented")
+	return 1
 }
 func d_add_d1(a, b float64) float64 {
-	panic("unimplemented")
+	return 1
+}
+
+func d_divide_d0(a, b float64) float64 {
+	return 1 / b
 }
 func d_divide_d1(a, b float64) float64 {
-	panic("unimplemented")
+	return -a / (b * b)
+}
+
+func d_multiply_d0(a, b float64) float64 {
+	return b
 }
 func d_multiply_d1(a, b float64) float64 {
-	panic("unimplemented")
+	return a
+}
+
+func d_subtract_d0(a, b float64) float64 {
+	return 1
 }
 func d_subtract_d1(a, b float64) float64 {
-	panic("unimplemented")
+	return -1
 }
+
 func d_pow_d0(a, b float64) float64 {
-	panic("unimplemented")
+	return b * math.Pow(a, b-1)
 }
 func d_pow_d1(a, b float64) float64 {
-	panic("unimplemented")
+	return math.Pow(a, b) * math.Log(a)
 }
 
 func d_abs_d0(a float64) float64 {
-	panic("d_abs_d0 unimplemented")
+	switch {
+	case a > 0:
+		return +1
+	case a < 0:
+		return -1
+	default:
+		return 0
+	}
 }
 func d_acos_d0(a float64) float64 {
-	panic("d_acos_d0 unimplemented")
+	return -1 / math.Sqrt(1-a*a)
 }
 func d_asin_d0(a float64) float64 {
-	panic("d_asin_d0 unimplemented")
+	return 1 / math.Sqrt(1-a*a)
 }
 func d_atan_d0(a float64) float64 {
-	panic("d_atan_d0 unimplemented")
+	return 1 / (1 + a*a)
 }
 func d_cos_d0(a float64) float64 {
-	panic("d_cos_d0 unimplemented")
+	return -math.Sin(a)
 }
 func d_cosh_d0(a float64) float64 {
-	panic("d_cosh_d0 unimplemented")
+	return math.Sinh(a)
 }
 func d_exp_d0(a float64) float64 {
-	panic("d_exp_d0 unimplemented")
+	return math.Exp(a)
 }
 func d_exp10_d0(a float64) float64 {
 	panic("d_exp10_d0 unimplemented")
 }
 func d_exp2_d0(a float64) float64 {
-	panic("d_exp2_d0 unimplemented")
+	return math.Log(2) * math.Pow(2, a)
 }
 func d_log_d0(a float64) float64 {
-	panic("d_log_d0 unimplemented")
+	return 1 / a
 }
 func d_log10_d0(a float64) float64 {
 	panic("d_log10_d0 unimplemented")
 }
+
+var cst_log2 float64 = math.Log(2)
+
 func d_log2_d0(a float64) float64 {
-	panic("d_log2_d0 unimplemented")
+	return 1 / (a * cst_log2)
 }
 func d_sin_d0(a float64) float64 {
-	panic("d_sin_d0 unimplemented")
+	return math.Cos(a)
 }
 func d_sinh_d0(a float64) float64 {
-	panic("d_sinh_d0 unimplemented")
+	return math.Cosh(a)
 }
 func d_sqrt_d0(a float64) float64 {
-	panic("d_sqrt_d0 unimplemented")
+	return 0.5 * math.Pow(a, -0.5)
 }
 func d_tan_d0(a float64) float64 {
-	panic("d_tan_d0 unimplemented")
+	return math.Pow(1/math.Cos(a), 2)
 }
 func d_tanh_d0(a float64) float64 {
-	panic("d_tanh_d0 unimplemented")
+	return 1 - math.Pow(math.Tanh(a), 2)
 }
